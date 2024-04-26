ee450a1aa9fcda21c2916afdb7ef1f9f70b46b1f
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-04-26 10:00:39 +0530

    login validate updated

--- javadb/PaymentWebApp/src/main/java/com/pack/dao/PaymentsWebAppDAO.java
+++ javadb/PaymentWebApp/src/main/java/com/pack/dao/PaymentsWebAppDAO.java
@@ -17,7 +17,7 @@ public class PaymentsWebAppDAO {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payments_web_app", "root",
 					"root");
 			Statement st = con.createStatement();
-			String storeUserDetailsQuery = "insert into User_info(User_Name,First_Name,Last_Name,Email_Id,Phone_Number,Date_Of_Birth,Address,Password) "
+			String storeUserDetailsQuery = "insert into User_info(User_Name, First_Name, Last_Name, Email_Id, Phone_Number, Date_Of_Birth, Address, Password) "
 					+ "values('" + userName + "','" + fName + "','" + lName + "','" + email + "','" + phNo + "','" + dob
 					+ "','" + address + "','" + pswd + "')";
 
@@ -32,25 +32,25 @@ public class PaymentsWebAppDAO {
 			e.printStackTrace();
 		}
 	}
-	
-	public static boolean loginValidate(String userNameOrPhoneNo, String password ) throws SQLException{
-		
+
+	public static boolean loginValidate(String userNameOrPhoneNo, String password) throws SQLException {
+
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payments_web_app", "root", "root");
-			String str="Durga_Smart_0012";
-
-			String loginValidateQuery = "SELECT * FROM User_info WHERE User_Name ='"+userNameOrPhoneNo+"' ";
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payments_web_app", "root",
+					"root");
 			Statement st = con.createStatement();
-		
+			String loginValidateQuery = "SELECT User_Name, Phone_Number, Password FROM user_info WHERE ((User_Name = '"
+					+ userNameOrPhoneNo + "' || Phone_Number = '" + userNameOrPhoneNo + "') && Password ='" + password
+					+ "')";
 			ResultSet rs = st.executeQuery(loginValidateQuery);
-			while(rs.next()){
+			while (rs.next()) {
 				System.out.println("Login Successfull !!");
 			}
 			con.close();
 			return true;
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
