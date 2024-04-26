618378ed8ae5734976a6f830dc168f37ab4f12d4
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-04-26 16:19:38 +0530

    Update SqlPaymentsWebAppQuery.sql

--- javadb/PaymentWebApp/src/main/java/com/pack/dao/SqlPaymentsWebAppQuery.sql
+++ javadb/PaymentWebApp/src/main/java/com/pack/dao/SqlPaymentsWebAppQuery.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE payments_web_app;
 
-use payments_web_app;
+USE payments_web_app;
 
 CREATE TABLE user_info(
 Id INT  PRIMARY KEY AUTO_INCREMENT,
@@ -14,10 +14,21 @@ Address VARCHAR(150) NOT NULL,
 Password VARCHAR(15) NOT NULL,
 Wallet_Balance DOUBLE NOT NULL DEFAULT "0"
 );
-desc user_info;
 
+DESC user_info;
 
-SELECT * FROM user_info;
+SELECT * FROM user_info;	
 -- drop table user_info;
 
 SELECT User_Name, Phone_Number, Password FROM user_info WHERE ((User_Name = "" || Phone_Number = "6303244920") && Password ="Pass");
+
+-- CREATE TABLE Bank_Acct_Details(
+-- Id INT PRIMARY KEY AUTO_INCREMENT,
+-- Acct_Num VARCHAR(30) UNIQUE NOT NULL,
+-- Acct_Holder_Name VARCHAR(50) NOT NULL,
+-- Bank_Name VARCHAR(40) NOT NULL,
+-- Acct_IFSC_Code VARCHAR(20) NOT NULL,
+-- Acct_Type ENUM("SAVINGS","CURRENT","LOAN","SALARY"),
+-- Acct_Pin VARCHAR(30) NOT NULL
+-- );
+
