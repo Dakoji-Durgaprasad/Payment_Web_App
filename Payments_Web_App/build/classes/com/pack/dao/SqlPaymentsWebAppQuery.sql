f68f6a5a37b33ebe8a68d43fd8896715b1cdf3a9
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-05-01 13:34:07 +0530

    initial commit

--- Payments_Web_App/build/classes/com/pack/dao/SqlPaymentsWebAppQuery.sql
+++ Payments_Web_App/build/classes/com/pack/dao/SqlPaymentsWebAppQuery.sql
@@ -1,10 +1,11 @@
+
 CREATE DATABASE payments_web_app;
 
 USE payments_web_app;
 
 CREATE TABLE user_info(
-Id INT  PRIMARY KEY AUTO_INCREMENT,
-User_Name VARCHAR(50) UNIQUE NOT NULL,
+Id INT UNIQUE NOT NULL AUTO_INCREMENT,
+User_Name VARCHAR(50)  PRIMARY KEY,
 First_Name VARCHAR(50) NOT NULL,
 Last_Name VARCHAR(50) NOT NULL,
 Email_Id VARCHAR(30) UNIQUE NOT NULL,
@@ -22,12 +23,38 @@ SELECT * FROM user_info;
 
 SELECT User_Name, Phone_Number, Password FROM user_info WHERE ((User_Name = "" || Phone_Number = "6303244920") && Password ="Pass");
 
--- CREATE TABLE Bank_Acct_Details(
--- Id INT PRIMARY KEY AUTO_INCREMENT,
--- Acct_Num VARCHAR(30) UNIQUE NOT NULL,
--- Acct_Holder_Name VARCHAR(50) NOT NULL,
--- Bank_Name VARCHAR(40) NOT NULL,
--- Acct_IFSC_Code VARCHAR(20) NOT NULL,
--- Acct_Type ENUM("SAVINGS","CURRENT","LOAN","SALARY"),
--- Acct_Pin VARCHAR(30) NOT NULL
--- );
+CREATE TABLE Bank_Account_Type(
+Id INT UNIQUE NOT NULL AUTO_INCREMENT,
+Acct_Type_Code VARCHAR(3) PRIMARY KEY,
+Acct_Type_Name VARCHAR(15)
+);
+INSERT INTO Bank_Account_Type(Id,Acct_Type_Code,Acct_Type_Name) 
+VALUES (1,"SA","SAVINGS"),
+(2,"CA","CURRENT"),
+(3,"LA","LOAN"),
+(4,"SL","SALARY");
+
+DESC Bank_Account_Type;
+SELECT * FROM Bank_Account_Type;
+
+
+CREATE TABLE Bank_Acct_Details(
+Id INT PRIMARY KEY AUTO_INCREMENT,
+Acct_Num VARCHAR(30) UNIQUE NOT NULL,
+Acct_Holder_Name VARCHAR(50) NOT NULL,
+Bank_Name VARCHAR(40) NOT NULL,
+Acct_IFSC_Code VARCHAR(20) NOT NULL,
+Acct_Pin VARCHAR(30) NOT NULL,
+Acct_Type VARCHAR(3) NOT NULL, 
+FOREIGN KEY(Acct_Type) REFERENCES Bank_Account_Type(Acct_Type_Code)
+ON UPDATE CASCADE
+ON DELETE CASCADE,
+User_Name  VARCHAR(50) NOT NULL, FOREIGN KEY(User_Name) REFERENCES user_info(User_Name),
+BanK_Balance DOUBLE NOT NULL DEFAULT "0",
+Phone_Num varchar(20) not null
+);
+
+select * from Bank_acct_details;
+
+-- alter table bank_acct_details 
+-- add column Phone_Num varchar(20) not null ;
